adefbb38a6b99143c4c5c08b7f6e5ea2f9f14766
= <mateusz.prusaczyk@gmail.com>
2017-11-18 11:58:29 +0100

    removed unneeded comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const config = require('./config/server-config');
-
-// const configureExpressApp = require('./server/configure-express-app');
-// const configureDb = require('./server/configure-db');
-
-// configureDb(config).then(db => {
-//     configureExpressApp(app, db, config);
-//     app.listen(config.PORT);
-//     console.log(`Magic happens on port ${config.PORT}`);
-// });
-
-// exports = module.exports = app;
 const path = require('path');
 const architect = require('architect');
 
